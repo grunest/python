b826bc3436c5e960f63875254bdae8e080bcc1d9
grunest <greg@grunest.com>
2017-11-29 02:06:58 -0800

    updated "docstring"

--- firstpython.py
+++ firstpython.py
@@ -1,7 +1,9 @@
-def add_it_up(x,y):
+""" This module does nothing really """
+def add_it_up(x, y):
+    """ Adds two numbers """
     return int(x) + int(y)
 
-one_number=input("Enter a number: ")
-second_number=input("Enter another number: ")
+one_number = input("Enter a number: ")
+second_number = input("Enter another number: ")
 print ("The sume of the two numbers is: " + str(add_it_up(int(one_number), int(second_number))))
 print ("This is a change")
