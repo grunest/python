21cd2ed02796f6ede8b5b28a9a5b2db4a103f7c4
grunest <greg@grunest.com>
2017-11-29 00:45:06 -0800

    Initial Store

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+def add_it_up(x,y):
+    return int(x) + int(y)
+
+x=input("Enter a number: ")
+y=input("Enter another number: ")
+print ("The sume of the two numbers is: " + add_it_up(x,y))
---
+++ test2.py
@@ -0,0 +1,6 @@
+print ("hello world!")
+msg = "This is next"
+if msg.isalpha:
+    print ("The variable is alpha")
+
+    
